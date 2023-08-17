e53575360f6c613a9b2f838d0f465a6a457c3ef5
aaqadir <aqansari47@gmail.com>
2023-08-17 13:35:04 +0530

    initial commit - setting up and running basic react app

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>Task Tracker Header</div>
+    // <header>
+    //   <h1>Task Tracker</h1>
+    // </header>
+  );
+};
+
+export default Header;
