75ce28778df5b083bdc5914cb03bd573a198dd06
aaqadir <aqansari47@gmail.com>
2023-08-19 13:37:52 +0530

    added few more funtionality and finishing touch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { React, useEffect, useState } from "react";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Task from "./components/TasksList";
 import AddTask from "./components/AddTask";
+import About from "./components/About";
+import Footer from "./components/Footer";
 
 function App() {
   const [showAddForm, setShowAddForm] = useState(false);
@@ -10,24 +13,23 @@ function App() {
   const apiUrl = "http://localhost:5000/tasks";
 
   useEffect(() => {
-    const getTasks = async () => {
-      const tasksFromServer = await fetchTasks();
+    const getAllTasks = async () => {
+      const tasksFromServer = await fetchAllTasks();
       setTasks(tasksFromServer);
     };
 
-    getTasks();
+    getAllTasks();
   }, []);
 
   // Fetch All Tasks
-  const fetchTasks = async () => {
+  const fetchAllTasks = async () => {
     const res = await fetch(apiUrl);
     const data = await res.json();
 
     return data;
   };
 
-  // Fetch Taks with id
-  // Fetch Task
+  // Fetch Task with id
   const fetchTask = async (id) => {
     const res = await fetch(`${apiUrl}/${id}`);
     const data = await res.json();
@@ -60,19 +62,12 @@ function App() {
     const res = await fetch(`${apiUrl}/${id}`, {
       method: "DELETE",
     });
-    setTasks(tasks.filter((task) => task.id !== id));
-  };
 
-  // Delete Task
-  // const deleteTask = async (id) => {
-  //   const res = await fetch(`${apiUrl}/${id}`, {
-  //     method: "DELETE",
-  //   });
-  //   //We should control the response status to decide if we will change the state or not.
-  //   res.status === 200
-  //     ? setTasks(tasks.filter((task) => task.id !== id))
-  //     : alert("Error Deleting This Task");
-  // };
+    //We should control the response status to decide if we will change the state or not.
+    res.status === 200
+      ? setTasks(tasks.filter((task) => task.id !== id))
+      : alert("Error in Deleting this task");
+  };
 
   //Toggle reminder
   const toggleReminderApp = async (id) => {
@@ -98,45 +93,38 @@ function App() {
     );
   };
 
-  // Toggle Reminder
-  const toggleReminder = async (id) => {
-    const taskToToggle = await fetchTasks(id);
-    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
-
-    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
-      method: "PUT",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(updTask),
-    });
-
-    const data = await res.json();
-
-    setTasks(
-      tasks.map((task) =>
-        task.id === id ? { ...task, reminder: data.reminder } : task
-      )
-    );
-  };
-
   return (
-    <div className="container">
-      <Header
-        onAdd={() => setShowAddForm(!showAddForm)}
-        showAdd={showAddForm}
-      />
-      {showAddForm && <AddTask onAdd={addTaskApp} />}
-      {tasks.length > 0 ? (
-        <Task
-          tasks={tasks}
-          onDelete={deleteTaskApp}
-          onToggle={toggleReminderApp}
+    <BrowserRouter>
+      <div className="container">
+        <Header
+          onAdd={() => setShowAddForm(!showAddForm)}
+          showAdd={showAddForm}
         />
-      ) : (
-        "No Task to display"
-      )}
-    </div>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                {showAddForm && <AddTask onAdd={addTaskApp} />}
+                {tasks.length > 0 ? (
+                  <Task
+                    tasks={tasks}
+                    onDelete={deleteTaskApp}
+                    onToggle={toggleReminderApp}
+                  />
+                ) : (
+                  "No Task to display"
+                )}
+              </>
+            }
+          ></Route>
+
+          <Route path="/about" element={<About></About>}></Route>
+        </Routes>
+
+        <Footer></Footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Button from "./Button";
+import { useLocation } from "react-router-dom";
 
 const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation();
   return (
     // <div>
     //   <div>Task Tracker Header</div>
@@ -10,11 +12,13 @@ const Header = ({ title, onAdd, showAdd }) => {
     // </div>
     <header className="header">
       <h1>{title}</h1>
-      <Button
-        color={showAdd ? "red" : "green"}
-        text={showAdd ? "Close" : "Add"}
-        onClickHeader={onAdd}
-      ></Button>
+      {location.pathname === "/" && (
+        <Button
+          color={showAdd ? "red" : "green"}
+          text={showAdd ? "Close" : "Add"}
+          onClickHeader={onAdd}
+        ></Button>
+      )}
     </header>
   );
 };
